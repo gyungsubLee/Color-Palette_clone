c05ada13b8e3a3164988403f7597f956fea095ff
gyungsubLee <gyungsub@DESKTOP-1JB0K0U.localdomain>
2022-09-22 16:44:24 +0900

    test Comiit

--- js/soundButton.js
+++ js/soundButton.js
@@ -1,7 +1,7 @@
 const soundButton = document.querySelector(".soundButton");
 const buttons = document.getElementsByClassName("main__color-item__copy-button");
 
-const audio = new Audio("../audio/버튼.wav");
+const audio = new Audio('../audio/button.mp3');
 
 let isSOn = true; 
 
@@ -13,6 +13,5 @@ const handleSoundBntText = () => {
     audio.play();
   }
 };
-
 
 soundButton.addEventListener('click', handleSoundBntText);
